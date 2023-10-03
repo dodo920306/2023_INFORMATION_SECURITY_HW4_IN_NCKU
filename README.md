227baa412dc4515578e3de192b893e1e473ad319
dodo920306 <dodo920306@gmail.com>
2023-10-03 10:45:20 +0800

    Finish the whole work
    A program has been successfully designed to produce right output
    according to users' input.
    It'll get correct keys in minutes.

---
+++ README.md
@@ -0,0 +1,64 @@
+# 2023_INFORMATION_SECURITY_HW4_IN_NCKU
+
+This is a repo for solving HW4 in 2023 INFORMATION_SECURITY courses in NCKU, in which a program should be designed to break an AES key according to known ciphertexts and parts of keys.
+
+# Prerequisites
+* Python 3.8.10
+* pip 20.0.2
+
+# Get Started
+To get started, you should clone this repo and enter it.
+
+```bash
+$ git clone https://github.com/dodo920306/2023_INFORMATION_SECURITY_HW4_IN_NCKU.git
+$ cd 2023_INFORMATION_SECURITY_HW4_IN_NCKU
+```
+
+Next, install pycryptodome in order to do AES
+
+```bash
+$ pip install pycryptodome
+```
+
+and install tqdm for multiprocessing.
+
+```bash
+$ pip install tqdm
+```
+
+Then you are good to go! Run
+
+```bash
+$ python main.py
+```
+
+, enter your student id in NCKU, and wait minutes to get the result!
+
+The following are the detail for the principle of this program.
+
+# Testing
+The problem includes AES in ECB mode with zeropadding. All using utf-8 as character encoding.
+
+In the beginning of the program, in order to check the encryption setting and the environment is functional, the program will use "123456789" in plaintext as key to encrypt the word "security". If every thing is OK, the ciphertext should be "pKjVPv28yVMn5cRXeUNYpg==".
+
+Next, likewise, in order to check the decryption setting and the environment is functional, the program will use "123456789" in plaintext as key to decrypt the previous ciphertext "pKjVPv28yVMn5cRXeUNYpg==". If every thing is OK, the result should be the original plaintext "security".
+
+# Input
+Then, the program will ask users to input their student id in NCKU, this should be a string with length 9 and a number as the last character. Any invalid input that doesn't follow these two rules will be blocked.
+In response to what number the last character in input moduloing 5 is, let us say that is x, the program will get the corresponding ciphertext and key according to the following table.
+| x | ciphertext | key |
+|-|-|-|
+|0|2NHkjlDyk82JBke5q8CnMQZ1iiHID8QEst+/Ld6lWFMP5omXXh/1LnmrYKOD04idKfzfL+6C96391/iN7+X0eg==|$"▆vXl▆K▆\/ {9Fp▆|
+|1|INNkAZHIpe5u9LvzhH24VyORcZQVDCFXzV6V/l9M7rpgqskMxvaRbGwR2dZaxMDZ|0lOS▆b] ▆&N) ▆w▆@+|
+|2|NnJyrVT80DxOU5jOxHdZ9NRlaLPRhaAUYANfaVACUeqcrPoXz5eeTs9m6X2fVJC9SJ+X03mu3zD/WTiUjwzIyg==|Bk▆fom] ▆H▆ (J▆'|,|
+|3|89NEvN56VtNjo1w5x3whmFUOZOqTaRyoMnIrPjCGKUv5n7kgGFHDmStzEgDFAU7QnZOK9MLeO/FW4etzIOhpKfOsw5xSD4Em72X1O2FRfaM=|2▆? ▆mYD;@▆;x▆v\"i|
+|4|FZp57a6p84EUNC7I/ENj4RhPZtryOJr4che9JbA8ng1eI8ZMTlsl8kzicBDqkOqkFj3lwC69KR2MeA8lscVlig==|\|q▆~k=▆&?I$Fx▆N▆|
+
+'▆' indicates unknown characters in keys. That's why we can't get the plaintexts.
+
+# Solving
+If every thing is OK, the program will start solving the key and the plaintext. 
+
+The way the program use is just brute-force. The program will iterate through every possible byte in ASCII to replacing each '▆'. Since there are four '▆' in every key, the program will run a 4-level for-loop in order to get the correct combination in $(2^8)^4$ possible ones.
+
+A progress bar is there to allow users to check if the program is still working.
---
+++ main.py
@@ -0,0 +1,104 @@
+from Crypto.Cipher import AES
+from tqdm.contrib.concurrent import process_map
+import base64
+import sys
+from time import monotonic
+import datetime
+
+def zeropadding(data, block_size):
+    add = len(data) % block_size
+    # BEHOLD! An if-statement that is so important that overlooking it led to me wasting TENS OF HOURS!
+    if add == 0:
+        return data
+    padding = b"\x00" * (block_size - add)
+    return data + padding
+
+def aes_encrypt_ecb(key, plaintext):
+    padded_key = zeropadding(key.encode('utf-8'), AES.block_size)
+    cipher = AES.new(padded_key, AES.MODE_ECB)
+    padded_data = zeropadding(plaintext.encode('utf-8'), AES.block_size)
+    ciphertext = cipher.encrypt(padded_data)
+    return base64.b64encode(ciphertext).decode('utf-8')
+
+def aes_decrypt_ecb(key, ciphertext):
+    padded_key = zeropadding(key.encode('utf-8'), AES.block_size)
+    cipher = AES.new(padded_key, AES.MODE_ECB)
+    ciphertext = base64.b64decode(ciphertext)
+    padded_data = cipher.decrypt(ciphertext)
+    return padded_data.rstrip(b"\x00").decode("utf-8")
+
+def worker(i):
+    main_key = key_set[int(student_id[8]) % 5]
+    for j in range(0x20, 0x7F):
+        for k in range(0x20, 0x7F):
+            for l in range(0x20, 0x7F):
+                try:
+                    key = main_key.replace('▆', chr(i), 1).replace('▆', chr(j), 1).replace('▆', chr(k), 1).replace('▆', chr(l), 1)
+                    plaintext = aes_decrypt_ecb(key, ciphertext)
+                    print("一可能合理金鑰為:", key)
+                    print("一可能合理明文為:", plaintext)
+                    # Since there could be many possible result that is valid, the loop doesn't end here.
+                except UnicodeDecodeError:
+                    pass
+
+# Check If the setting is good. 
+# Test pre-setting
+key = '123456789'
+plaintext = "security"
+
+ciphertext = aes_encrypt_ecb(key, plaintext)
+print("測試密文結果為:", ciphertext) # It should be "pKjVPv28yVMn5cRXeUNYpg==".
+if ciphertext != "pKjVPv28yVMn5cRXeUNYpg==":
+    sys.exit("wrong ciphertext.")
+else:
+    print("加密功能 OK!")
+
+plaintext = aes_decrypt_ecb(key, ciphertext)
+print("測試明文結果為:", plaintext) # It should be "security".
+if plaintext != "security":
+    sys.exit("wrong plaintext.")
+else:
+    print("解密功能 OK!")
+
+# The question set
+ciphertext_set = {
+    0: "2NHkjlDyk82JBke5q8CnMQZ1iiHID8QEst+/Ld6lWFMP5omXXh/1LnmrYKOD04idKfzfL+6C96391/iN7+X0eg==",
+    1: "INNkAZHIpe5u9LvzhH24VyORcZQVDCFXzV6V/l9M7rpgqskMxvaRbGwR2dZaxMDZ",
+    2: "NnJyrVT80DxOU5jOxHdZ9NRlaLPRhaAUYANfaVACUeqcrPoXz5eeTs9m6X2fVJC9SJ+X03mu3zD/WTiUjwzIyg==",
+    3: "89NEvN56VtNjo1w5x3whmFUOZOqTaRyoMnIrPjCGKUv5n7kgGFHDmStzEgDFAU7QnZOK9MLeO/FW4etzIOhpKfOsw5xSD4Em72X1O2FRfaM=",
+    4: "FZp57a6p84EUNC7I/ENj4RhPZtryOJr4che9JbA8ng1eI8ZMTlsl8kzicBDqkOqkFj3lwC69KR2MeA8lscVlig=="
+}
+
+while True:
+    student_id = input("------------------------\n請輸入您的成大學號: ") # my id
+    try:
+        if len(student_id) == 9:
+            ciphertext = ciphertext_set[int(student_id[-1]) % 5]
+            break
+        else:
+            print("學號格式並不正確，請您再試一次。")
+    except ValueError:
+        print("學號格式並不正確，請您再試一次。")
+    except IndexError:
+        print("學號格式並不正確，請您再試一次。")
+
+print("所屬密文為:", ciphertext)
+
+key_set = {
+    0: "$\"▆vXl▆K▆\/ {9Fp▆",
+    1: "0lOS▆b] ▆&N) ▆w▆@+",
+    2: "Bk▆fom] ▆H▆ (J▆'|,",
+    3: "2▆? ▆mYD;@▆;x▆v\"i",
+    4: "|q▆~k=▆&?I$Fx▆N▆"
+}
+key = key_set[int(student_id[8]) % 5]
+print("所屬金鑰為:", key)
+
+# Start solving
+print("破解開始！")
+start = monotonic()
+process_map(worker, range(0x20, 0x7F), max_workers=4)
+end = monotonic()
+SecToConvert = end - start
+ConvertedSec = str(datetime.timedelta(seconds = SecToConvert))
+print("所耗費總時長為:", ConvertedSec)
